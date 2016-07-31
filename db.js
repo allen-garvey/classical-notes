d486ddf678feeda7b31f44c01ff68b3fc09dcdcd
Allen Garvey <allengarvey@hotmail.com>
2016-07-31 14:45:45 -0400

    Add database configuration for elastic beanstalk

--- db.js
+++ db.js
@@ -1,8 +1,21 @@
-var db = {
-	host  : 'localhost',
- 	user  : 'cn_user',
-  	password: '',
-  	database: 'classical_notes'
-};
+"use strict";
+
+//elastic beanstalk
+if(process.env.RDS_HOSTNAME){
+	var db = {
+				host     : process.env.RDS_HOSTNAME,
+				user     : process.env.RDS_USERNAME,
+				password : process.env.RDS_PASSWORD,
+				port     : process.env.RDS_PORT
+			};
+}
+else{
+	var db = {
+				host  : 'localhost',
+				user  : 'cn_user',
+				password: '',
+				database: 'classical_notes'
+			};
+}
 
 module.exports = db;
