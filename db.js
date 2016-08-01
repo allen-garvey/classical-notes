63de208369556773fb87e933573bc168402ec5e9
Allen Garvey <allengarvey@hotmail.com>
2016-07-31 21:02:56 -0400

    Refactor database config code to reduce duplication

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 "use strict";
 
-//elastic beanstalk db configuration
-if(process.env.RDS_HOSTNAME){
-	var db = {
-				host     : process.env.RDS_HOSTNAME,
-				user     : process.env.RDS_USERNAME,
-				password : process.env.RDS_PASSWORD,
-				port     : process.env.RDS_PORT,
-				database : 'classical_notes'
-			};
-}
 //local db configuration
-else{
-	var db = {
+var local_db = {
 				host  : 'localhost',
 				user  : 'cn_user',
 				password: '',
 				database: 'classical_notes'
-			};
+};
+
+//process.env is for elastic beanstalk db configuration
+var db = {
+			host     : process.env.RDS_HOSTNAME || local_db.host,
+			user     : process.env.RDS_USERNAME || local_db.user,
+			password : process.env.RDS_PASSWORD || local_db.password,
+			database : local_db.database
+};
+if(process.env.RDS_PORT){
+	db.port = process.env.RDS_PORT;
 }
+
 
 module.exports = db;
